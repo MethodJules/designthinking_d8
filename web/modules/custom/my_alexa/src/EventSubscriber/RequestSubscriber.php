9ef196821c222f96b7b6a6941d44892aac9ddf60
MethodJules <hoferj@uni-hildesheim.de>
2019-12-12 14:21:12 +0100

    changed my alexa module

--- web/modules/custom/my_alexa/src/EventSubscriber/RequestSubscriber.php
+++ web/modules/custom/my_alexa/src/EventSubscriber/RequestSubscriber.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace \Drupal\my_alexa\EventSubscriber;
+namespace Drupal\my_alexa\EventSubscriber;
 use Drupal\alexa\AlexaEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -10,6 +10,8 @@ class RequestSubscriber implements EventSubscriberInterface {
      */
     public static function getSubscribedEvents()
     {
+	$message = 'ddhdhdhd';
+	\Drupal::logger('my_alexa')->notice($message);
         $events['alexaevent.request'][] = ['onRequest', 0];
         return $events;
     }
@@ -21,16 +23,19 @@ class RequestSubscriber implements EventSubscriberInterface {
      public function onRequest(AlexaEvent $event) {
          $request = $event->getRequest();
          $response = $event->getResponse();
-
+	//\Drupal::logger('my_alexa')->notice('geht in die Methode on Request');
          switch($request->intentName) {
              case 'AMAZON.HelpIntent':
                 $response->respond('Du kann nach einer Methode fragen.');
                 break;
              case 'Methode_abfragen':
                 $response->respond('Die Methode lautet pffff.');
+		\Drupal::logger('my_alexa')->notice('Alexa Aufruf');
+		break;
              default:
                 $response->respond('Hallo Du. Ich kann dir Methoden vorstellen.');
-                break;
+                \Drupal::logger('my_alexa')->notice('ddddsdssssssssss');
+		break;
          }
      }
 }
